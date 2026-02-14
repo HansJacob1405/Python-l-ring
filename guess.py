676be7391a4a2651b756393dd6851f43b7807acf
HansJacob1405 <hj_jacobsen@hotmail.com>
2026-02-14 17:21:05 +0100

    Refactor guessing game into functions.First attempt

--- guess.py
+++ guess.py
@@ -1,33 +1,53 @@
 import random
 
-secret = random.randint(1, 20)
-attempts = 0
 
-print("Jeg tænker på et tal mellem 1 og 20.")
+def generate_secret():
+    return random.randint(1, 20)
 
-max_attempts = 6
 
-while attempts < max_attempts:
-    guess_str = input("Gæt et tal: ").strip()
+def get_guess():
 
-    if not guess_str.isdigit():
-        print("Skriv et helt tal mellem 1 og 20, fx 7.")
-        continue
+    attempts = 0
 
-    guess = int(guess_str)
+    max_attempts = 6
 
-    if not (1 <= guess <= 20):
-        print("Tallet skal være mellem 1 og 20.")
-        continue
+    secret = generate_secret()
 
-    attempts += 1
+    print(f"Det hemmelige tal er {secret}")
 
-    if guess < secret:
-        print("For lavt!")
-    elif guess > secret:
-        print("For højt!")
+    print("Jeg tænker på et tal mellem 1 og 20.")
+
+    while attempts < max_attempts:
+        guess_str = input("Gæt et tal: ").strip()
+
+        if not guess_str.isdigit():
+            print("Skriv et helt tal mellem 1 og 20, fx 7.")
+            continue
+
+        guess = int(guess_str)
+
+        if not (1 <= guess <= 20):
+            print("Tallet skal være mellem 1 og 20.")
+            continue
+
+        attempts += 1
+
+        if guess < secret:
+            print("For lavt!")
+        elif guess > secret:
+            print("For højt!")
+        else:
+            print(f"Korrekt! Du brugte {attempts} forsøg.")
+            break
     else:
-        print(f"Korrekt! Du brugte {attempts} forsøg.")
-        break
-else:
-    print(f"Du har haft {max_attempts} forsøg, så du får ikke flere. Det rigtige tal var {secret}.")
+        print(f"Du har haft {max_attempts} forsøg, så du får ikke flere. Det rigtige tal var {secret}.")
+
+
+def play_game():
+    get_guess()
+
+
+play_game()
+
+
+
