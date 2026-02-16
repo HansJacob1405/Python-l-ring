1f61858b6cb47e4b42184219aace5157babdfbc1
HansJacob1405 <hj_jacobsen@hotmail.com>
2026-02-16 11:28:18 +0100

    Adding parameters to functions

--- guess.py
+++ guess.py
@@ -1,47 +1,45 @@
 import random
 
 
-def generate_secret():
-    return random.randint(1, 20)
+def generate_secret(low,high):
+    return random.randint(low, high)
 
 
-def get_guess():
+def get_guess(low,high):
 
     guess_given = False
 
     while not guess_given:
-    
+
         guess_str = input("Gæt et tal: ").strip()
 
         try:  
             guess = int(guess_str)
-            guess_given = True
-            return int(guess_str)
+            
+            if not (low <= guess <= high):
+             print("Tallet skal være mellem {low} og {high}.")
+             continue
+            else: 
+             guess_given = True
+             return int(guess_str)
         except ValueError:
-            print("Skriv et helt tal mellem 1 og 20, fx 7.")
+            print("Skriv et helt tal mellem {low} og {high}")
             continue
 
-    
-def play_game():
+def play_game(low,high,max_attempts):
 
     attempts = 0
 
-    max_attempts = 6
-
-    secret = generate_secret()
+    secret = generate_secret(low,high)
 
     print(f"Det rigtige tal var {secret}.")
 
-    print("Jeg tænker på et tal mellem 1 og 20.")
+    print("Jeg tænker på et tal mellem {low} og {high}.")
 
     while attempts < max_attempts:
 
-        guess = get_guess()
+        guess = get_guess(low,high)
          
-        if not (1 <= guess <= 20):
-            print("Tallet skal være mellem 1 og 20.")
-            continue
-
         attempts += 1
 
         if guess < secret:
@@ -55,4 +53,13 @@ def play_game():
         print(f"Du har haft {max_attempts} forsøg, så du får ikke flere. Det rigtige tal var {secret}.")
 
 
-play_game()
+if __name__ == "__main__":
+
+    low=1
+    high = 20
+    max_attempts =6
+
+    play_game(low, high, max_attempts)
+
+
+
